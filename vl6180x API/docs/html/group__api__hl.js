e36e280a49d344a16f82d53f99b989f2b0460316
Unknown <grenblot@gmail.com>
2018-02-14 20:53:41 +0100

    removed unused folder + updated readme

--- vl6180x API/docs/html/group__api__hl.js
+++
@@ -1,6 +0,0 @@
-var group__api__hl =
-[
-    [ "Init functions", "group__api__init.html", "group__api__init" ],
-    [ "Ranging functions", "group__api__hl__range.html", "group__api__hl__range" ],
-    [ "ALS functions", "group__api__hl__als.html", "group__api__hl__als" ]
-];
