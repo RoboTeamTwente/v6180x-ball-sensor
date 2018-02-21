f0198e8d73a3cb27909d9d04ee13e50372b38aeb
Unknown <grenblot@gmail.com>
2018-02-21 10:28:51 +0100

    divided settings into private & custom

--- testproject/Src/ballsensor.c
+++ testproject/Src/ballsensor.c
@@ -38,7 +38,33 @@ void RdByte(uint16_t index, uint8_t* data) {
 	}
 }
 
-void LoadSettings() {
+void LoadCustomSettings() {
+	// Recommended : Public registers - See data sheet for more detail
+		WrByte(SYSTEM_MODE_GPIO1,0x30); //set GPIO1 to interrupt output, active high
+		//WrByte(0x0011, 0x10); // Enables polling for ‘New Sample ready’
+		// when measurement completes
+		WrByte(0x010a, 0x30); // Set the averaging sample period
+		// (compromise between lower noise and
+		// increased execution time)
+		WrByte(0x003f, 0x46); // Sets the light and dark gain (upper
+		// nibble). Dark gain should not be
+		// changed.
+		WrByte(0x0031, 0xFF); // sets the # of range measurements after
+		// which auto calibration of system is
+		// performed
+		WrByte(0x0040, 0x63); // Set ALS integration time to 100ms
+		WrByte(0x002e, 0x01); // perform a single temperature calibration
+		// of the ranging sensor
+		WrByte(SYSRANGE_INTERMEASUREMENT_PERIOD, 0x00); // Set default ranging inter-measurement
+		// period to 10ms
+		WrByte(0x003e, 0x31); // Set default ALS inter-measurement period
+		// to 500ms
+		WrByte(SYSTEM_INTERRUPT_CONFIG_GPIO, 0x24); // Configures interrupt on ‘New Sample
+		// Ready threshold event’
+}
+
+void LoadPrivateSettings() {
+	//do not modify these, taken from application note
 	WrByte(0x0207, 0x01);
 	WrByte(0x0208, 0x01);
 	WrByte(0x0096, 0x00);
@@ -69,30 +95,7 @@ void LoadSettings() {
 	WrByte(0x01ac, 0x3e);
 	WrByte(0x01a7, 0x1f);
 	WrByte(0x0030, 0x00);
-	// Recommended : Public registers - See data sheet for more detail
-	WrByte(SYSTEM_MODE_GPIO1,0x30);
-	//WrByte(0x0011, 0x10); // Enables polling for ‘New Sample ready’
-	// when measurement completes
-	WrByte(0x010a, 0x30); // Set the averaging sample period
-	// (compromise between lower noise and
-	// increased execution time)
-	WrByte(0x003f, 0x46); // Sets the light and dark gain (upper
-	// nibble). Dark gain should not be
-	// changed.
-	WrByte(0x0031, 0xFF); // sets the # of range measurements after
-	// which auto calibration of system is
-	// performed
-	WrByte(0x0040, 0x63); // Set ALS integration time to 100ms
-	WrByte(0x002e, 0x01); // perform a single temperature calibration
-	// of the ranging sensor
-	WrByte(SYSRANGE_INTERMEASUREMENT_PERIOD, 0x00); // Set default ranging inter-measurement
-	// period to 10ms
-	WrByte(0x003e, 0x31); // Set default ALS inter-measurement period
-	// to 500ms
-	//WrByte(SYSTEM_MODE_GPIO1,0x08); //set GPIO1 to interrupt output, active high
-	//WrByte(SYSTEM_MODE_GPIO1,0x10); //set GPIO1 to interrupt output, active high
-	WrByte(SYSTEM_INTERRUPT_CONFIG_GPIO, 0x24); // Configures interrupt on ‘New Sample
-	// Ready threshold event’
+
 
 }
 
@@ -120,7 +123,8 @@ void initializeDevice() {
 		uprintf("reset: %d\r\n", reset);
 
 	//LOAD A BUNCH OF SETTINGS ONTO DEVICE
-	LoadSettings();
+	LoadPublicSettings();
+	LoadPrivateSettings();
 	if(STATUS_DEBUG)
 		uprintf("settings loaded\r\n");
 
@@ -133,7 +137,7 @@ void measureRange()
 {
 	initializeDevice();
 
-	uint8_t status, range_status;
+	uint8_t range_status;
 
 	// Start a range measurement
 	while (!((range_status) & 0x01)){
